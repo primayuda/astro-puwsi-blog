5dea061d8cd112b9da7bf9dbea255f2924708979
primayuda <aprita.primayuda@gmail.com>
2023-02-24 11:19:30 +0700

    adding team page

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Blog Perkumpulan Usaha Wisata Selam Indonesia";
+export const SITE_TITLE = "Perkumpulan Usaha Wisata Selam Indonesia";
 export const SITE_DESCRIPTION =
-  "Selamat datang di blog PUWSI.";
+  "Selamat datang di website PUWSI.";
 export const TWITTER_HANDLE = "@yourtwitterhandle";
-export const MY_NAME = "PUWDI";
+export const MY_NAME = "PUWSI";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
